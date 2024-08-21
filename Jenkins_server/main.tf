5842a2c0ae007c8dd979f817c78618acdfaeed87
Sarthak221198 <sarthakmamgain44@gmail.com>
2024-08-22 01:40:00 +0530

    pushing new changes

--- Jenkins_server/main.tf
+++ Jenkins_server/main.tf
@@ -5,8 +5,8 @@ module "vpc" {
   name = "jenkins-vpc"
   cidr = var.vpc_cidr
 
-  azs            = data.aws_availability_zones.azs.names
-  public_subnets = var.public_subnets
+  azs                     = data.aws_availability_zones.azs.names
+  public_subnets          = var.public_subnets
   map_public_ip_on_launch = true
 
   enable_dns_hostnames = true
@@ -65,21 +65,21 @@ module "sg" {
 #EC2
 
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
 
   name = "jenkins-instance"
 
-  instance_type          = var.instance_type
-  key_name               = "Practice"
-  monitoring             = true
-  vpc_security_group_ids = [module.sg.security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
+  instance_type               = var.instance_type
+  key_name                    = "Practice"
+  monitoring                  = true
+  vpc_security_group_ids      = [module.sg.security_group_id]
+  subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  user_data = filebase64("jenkins-installation.sh")
-  availability_zone = data.aws_availability_zones.azs.names[0]
+  user_data                   = filebase64("jenkins-installation.sh")
+  availability_zone           = data.aws_availability_zones.azs.names[0]
 
   tags = {
-    Name = "Jenkins-Server"
+    Name        = "Jenkins-Server"
     Terraform   = "true"
     Environment = "dev"
   }
